258f9db3d5cf1d93954e0fc8291d24cce7271b98
chisaelim <chisae0123@gmail.com>
2026-09-02 15:11:08 +0700

    frontend pages

--- vuejs-app/src/components/pages/Dashboard.vue
+++ vuejs-app/src/components/pages/Dashboard.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>Dashboard</h1>
+  <div class="alert alert-success" role="alert">
+    You are logged in successfully. This is your dashboard.
+    <RouterLink :to="{ name: 'auth.signout' }"><i class="fas fa-sign-out-alt text-danger"></i></RouterLink>
+  </div>
 </template>
