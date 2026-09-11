16fc5de481ef0ec72bc891f4b3adb6b3def7074c
chisaelim <chisae0123@gmail.com>
2026-09-11 14:25:32 +0700

    backend for reset password

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -12,6 +12,12 @@ Route::get('/verify/email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->name('verify.email');
 Route::post('/send/verification-email', [AuthController::class, 'sendVerificationEmail']);
 
+// 1
+Route::post('/send/reset-password-email', [AuthController::class, 'sendResetPasswordEmail']);
+
+// 2
+Route::post('/set/new-password', [AuthController::class, 'setNewPassword'])->name('set.new-password');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/signout', [AuthController::class, 'signout']);
     Route::get('/verify', [AuthController::class, 'verify']);
